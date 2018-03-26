383fde479904f0875eb65050a4f02b7fda2c55b8
Sheikh-Zubair <37775802+Sheikh-Zubair@users.noreply.github.com>
2018-03-26 12:02:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const path = require('path'); // a buitin module 
 const app = express(); 
+// port
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 // Body parser middle where
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
@@ -16,9 +19,8 @@ app.get('/', (req, res) => {
 });
 // listen to port 3000
 // the function/method below runs whenever someone trires to reach this system on port 3000
-app.listen(3000, () => {
-    console.log('Express server started on port 3000');
-});
+app.listen(port,ip);
+module.exports = app ;
 
 
 
